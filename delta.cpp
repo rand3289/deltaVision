c33a4dbd9740ee7d57dc0437604c342ca600e9a8
rand3289 <rand3289@rand3289.com>
2020-12-16 00:36:33 -0500

    editing 7

--- delta.cpp
+++ delta.cpp
@@ -1,23 +1,22 @@
-// This program requires OpenCV library to compile and link
+// Displays how video looks when only changes in pixel values and not values themselves are used.
+// This program requires OpenCV library to compile and link.
 #include <opencv2/opencv.hpp>
 #include <iostream>
-#include <fstream>
-#include <iomanip> // format stream printing
-#include <vector>
-#include <algorithm>
+#include <iomanip> // format stream IO
+#include <cstring> // memset()
 #include <chrono>
 using namespace std;
 using namespace cv;
 
 
-struct FiberInfo {
+struct WinInfo {
     int algorithm = 0;
 };
 
 
 void mouse(int event, int x, int y, int flags, void* userdata){
     if( EVENT_LBUTTONUP != event ) { return; }
-    FiberInfo* data = static_cast<FiberInfo*>(userdata);
+    WinInfo* data = static_cast<WinInfo*>(userdata);
     ++data->algorithm;
 }
 
@@ -32,7 +31,7 @@ int getVideoSrcCount(cv::VideoCapture& cam){ // how many video sources are there
 }
 
 
-double getFps(){
+double getFps(){ // frames per second
 	static int count = 0;
 	static double fps = 0;
 	static auto start = std::chrono::steady_clock::now();
@@ -53,6 +52,10 @@ int main(int argc, char* argv[]){
     cout << "######## Found " << camCount << " video sources" << endl;
     cout.flush();
     cam.open( camCount-1 ); // convert device count to the largest existing zero based device index and open it
+    if( !cam.isOpened() ){
+        cerr << "ERROR: could not open camera." << endl;
+        exit(1);
+    }
 
     const char dataWin[] = "original";
     namedWindow(dataWin);
@@ -62,38 +65,27 @@ int main(int argc, char* argv[]){
     namedWindow(deltaWin);
     moveWindow(deltaWin,  600, 300);
 
-    FiberInfo info1; // for each window
-    FiberInfo info2;
+    WinInfo info1; // one for each window
+    WinInfo info2;
     setMouseCallback(dataWin,  mouse, (void*)&info1); 
     setMouseCallback(deltaWin, mouse, (void*)&info2);
-
-    const cv::Scalar colorW = CV_RGB(255,255,255);
 
     cv::Mat img;
     cv::Mat imgGrey;
     cv::Mat imgPrev;
     cv::Mat imgOut;
 
-    if( !cam.isOpened() ){
-        cerr << "ERR: could not open camera." << endl;
-        exit(1);
-    }
-
-    if( cam.read(img) ){
+    if( cam.read(img) ){ // init two greyscale Mat imgOut and imgPrev
         cv::cvtColor(img, imgOut, cv::COLOR_BGR2GRAY);
         imgPrev = imgOut.clone();
         imgPrev = 0; // clear the image
     } else {
-        cerr << "ERR: could not read an image from camera." << endl;
+        cerr << "ERROR: could not read an image from camera." << endl;
         exit(2);
     }
 
     double data [img.rows][img.cols]; // use the resolution of an aquired image
-    for(int y =0; y < img.rows; ++y){
-        for(int x=0; x<img.cols; ++x){
-           data[y][x] = 0;
-        }
-    }
+    memset(data, 0, sizeof(data));
 
     while( cam.read(img) ){
         cv::cvtColor(img, imgGrey, cv::COLOR_BGR2GRAY); // convert to grayscale
@@ -112,14 +104,10 @@ int main(int argc, char* argv[]){
                         data[y][x] += abs(delta); // dt starts with UNINITIALIZED (random) data !!!
                         pix = data[y][x];
                         data[y][x] = pix / 2;
-//                        data[y][x] = pix - (pix / 2);
-//                        pix *= 2;
-//                        pix = 255 - pix;
                         break;
-//                    case : pix = 127+delta; break;
-//                    case : pix = -delta; checkBounds = false; break;
 		}
-		if(checkBounds){ // allow overflow?
+
+		if(checkBounds){ // allow 8 bit overflow?
                     pix = pix > 255 ? 255 : (pix < 0 ? 0 : pix); 
                 }
                 imgOut.at<uchar>(y,x) = pix;
@@ -131,7 +119,8 @@ int main(int argc, char* argv[]){
         stringstream ss;
 	ss << "Q to quit.  algorithm=" << info2.algorithm << "  fps=" << std::setprecision(3) << getFps();
 	const cv::Point xy(5,img.rows-10);
-        Mat curr = info1.algorithm % 2 ? img : imgGrey; // algorithm is changed by mouse click
+        const cv::Scalar colorW = CV_RGB(255,255,255);
+        Mat curr = info1.algorithm % 2 ? img : imgGrey; // output is changed by left mouse click
 	cv::putText(curr, ss.str(), xy, 0, 0.8, colorW);
         imshow(dataWin, curr);
         imshow(deltaWin, imgOut);
